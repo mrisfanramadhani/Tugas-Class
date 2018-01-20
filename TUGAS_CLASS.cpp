1eeb22c7673b515eb436c09215db1a7a53453ad1
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2018-01-20 11:56:46 +0800

    OUTPUT and FINISH

--- TUGAS_CLASS.cpp
+++ TUGAS_CLASS.cpp
@@ -8,4 +8,28 @@ class MOTOR{
 	string merk,nama;
 
 };
+int main(){
+	
+	MOTOR NINJA,CBR,THUNDER;
+	
+	NINJA.nama    ="Motor Ninja ";
+	NINJA.merk = " Kawasaki ";
+	CBR.nama       = "Motor CBR ";
+	CBR.merk   = " Honda ";
+	THUNDER.merk = " Suzuki ";
+
+
+	cout << (NINJA.nama);
+	cout << (NINJA.merk);
+    NINJA.lokasi("DEALER");
+    
+    cout<< (CBR.nama);
+    cout << (CBR.merk);
+    CBR.lokasi("SHOWROOM");
+
+
+}
+void MOTOR::lokasi(string letak){
+	cout<<"ada di "<<letak<<"!!"<<endl;
+}
 
